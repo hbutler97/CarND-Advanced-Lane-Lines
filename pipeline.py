6eee69a6d635cb18da4e3c2e4392bc5c198a2bb5
Howard Butler <howard.butler@intel.com>
2017-07-29 20:05:18 -0700

    finished with thresholds

--- pipeline.py
+++ pipeline.py
@@ -22,23 +22,32 @@ def bgr_to_rgb(image):
     b,g,r = cv2.split(image)
     return cv2.merge([r,g,b])
 
-def compare_images(image1, image2):
+def compare_images(image1, image2, cmap=None):
     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
     f.tight_layout()
-    ax1.imshow(image1)
+    ax1.imshow(image1, cmap=cmap)
     ax1.set_title('Original Image', fontsize=50)
-    ax2.imshow(image2)
+    ax2.imshow(image2, cmap=cmap)
     ax2.set_title('Processed Image', fontsize=50)
     plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
     plt.show()
-def calibrate_camera(nx, ny, images):
+def calibrate_camera(nx, ny):
     objpoints = [] #3D points in real world space
     imgpoints = [] #2D points in image plane
-    gray_image = cv2.cvtColor(images[0], cv2.COLOR_BGR2GRAY)
-    gray_shape = gray_image.shape[::-1]
+
     # Prepare object points like (0,0,0, (1,0,0), (2,0,0)....,(7,4,0)
     objp = np.zeros((nx*ny, 3), np.float32)
     objp[:,:2] = np.mgrid[0:nx,0:ny].T.reshape(-1,2)
+
+    images = []
+    image_files = glob.glob('./camera_cal/calibration*.jpg')
+
+    for image_file in image_files:
+        images.append(bgr_to_rgb(cv2.imread(image_file)))
+        
+    gray_image = cv2.cvtColor(images[0], cv2.COLOR_BGR2GRAY)
+    gray_shape = gray_image.shape[::-1]
+
 
     for image in images:
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -51,21 +60,133 @@ def calibrate_camera(nx, ny, images):
             ret = False
 
     return cv2.calibrateCamera(objpoints, imgpoints, gray_shape, None, None)
+
+def abs_sobel_thresh(image, orient='x', thresh=(0,255)):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    if orient == 'x':
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+    elif orient == 'y':
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+    else:
+        print("bad orient passed")
+        exit()
+    abs_sobel = np.absolute(sobel)
+    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+    sbinary = np.zeros_like(scaled_sobel)
+    sbinary[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+    return sbinary
+
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # 3) Calculate the magnitude 
+    gradmag = np.sqrt(sobelx**2 + sobely**2)
+    # 4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
+    scale_factor = np.max(gradmag)/255 
+    gradmag = (gradmag/scale_factor).astype(np.uint8) 
+    binary_output = np.zeros_like(gradmag)
+    binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
+    # 5) Create a binary mask where mag thresholds are met
+    # 6) Return this mask as your binary_output image
+    
+    return binary_output
+
+def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
     
 
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # 3) Take the absolute value of the x and y gradients
+    abs_sobelx = np.absolute(sobelx)
+    abs_sobely = np.absolute(sobely)
+    # 4) Use np.arctan2(abs_sobely, abs_sobelx) to calculate the direction of the gradient
+    direction = np.arctan2(abs_sobely, abs_sobelx)
+    
+    
+    binary_output =  np.zeros_like(direction)
+    binary_output[(direction >= thresh[0]) & (direction <= thresh[1])] = 1
+    
+    # 5) Create a binary mask where direction thresholds are met
+    # 6) Return this mask as your binary_output image
+    
+    return binary_output
 
-calibration_images = []
-image_files = glob.glob('./camera_cal/calibration*.jpg')
+def color_rgb_threshold(image, channel='r', thresh=(0,255)):
+    R = image[:,:,0]
+    G = image[:,:,1]
+    B = image[:,:,2]
+    if channel == 'r':
+        convert_channel = R
+    elif channel == 'G':
+        convert_channel = G
+    elif channel == 'b':
+        convert_channel = b
+    else:
+        print("bad channel")
+        exit()
+    binary = np.zeros_like(convert_channel)
+    binary[(convert_channel > thresh[0]) & (convert_channel <= thresh[1])] = 1
+    return binary
+
+def color_hls_threshold(image, channel='h', thresh=(0,255)):
+    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+    H = hls[:,:,0]
+    L = hls[:,:,1]
+    S = hls[:,:,2]
+    if channel == 'h':
+        convert_channel = H
+    elif channel == 'l':
+        convert_channel = L
+    elif channel == 's':
+        convert_channel = S
+    else:
+        print("bad channel")
+        exit()
+    binary = np.zeros_like(convert_channel)
+    binary[(convert_channel > thresh[0]) & (convert_channel <= thresh[1])] = 1
+    return binary
+
+#Main
+#1. Calibrate Camera
+ret, mtx, dist, rvecs, tvecs = calibrate_camera(9, 6)
+
+
+#test code
+
+images = []
+image_files = glob.glob('./test_images/*.jpg')
 
 for image_file in image_files:
-    calibration_images.append(cv2.imread(image_file))
+    images.append(bgr_to_rgb(cv2.imread(image_file)))
+        
+for image in images:
+    new_image = cv2.undistort(image, mtx, dist, None, mtx)
+    xsobel = abs_sobel_thresh(new_image,'x', (20, 100))
+    ysobel = abs_sobel_thresh(new_image,'y', (20, 100))
+    magthresh =  mag_thresh(new_image,3, (30, 100))
+    dirthresh = dir_threshold(image, sobel_kernel=15, thresh=(0.7, 1.3))
+    combined = np.zeros_like(dirthresh)
+    combined[((xsobel == 1) & (ysobel == 1)) | ((magthresh == 1) & (dirthresh == 1))] = 1
 
+    r =  color_rgb_threshold(image, channel='r', thresh=(200,255))
+    s =  color_hls_threshold(image, channel='s', thresh=(90,255))
 
+    color_combined =np.zeros_like(r)
+    color_combined[(r == 1) & (s == 1)] = 1
+    combined_binary = np.zeros_like(color_combined)
+    combined_binary[(color_combined == 1) | (combined == 1)] = 1
 
-ret, mtx, dist, rvecs, tvecs = calibrate_camera(9, 6, calibration_images)
-
-for image in calibration_images:
-    compare_images(bgr_to_rgb(image), cv2.undistort(bgr_to_rgb(image), mtx, dist, None, mtx))
+    color_binary = np.dstack(( np.zeros_like(combined), combined, color_combined))
+    compare_images(combined_binary, color_binary, cmap='gray')
 
 
 '''
